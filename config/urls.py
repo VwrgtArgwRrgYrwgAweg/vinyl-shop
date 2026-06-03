01fac2fca9ba3b85ea6632529f1ebfeab198040d
IvanIvanovich] <2743@mail.ru>
2026-06-03 14:09:21 +0500

    Add media settings

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +16,4 @@ urlpatterns = [
     path('delivery/', views.delivery, name='delivery'),
     path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
