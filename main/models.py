e92a4d8de737fdb415a388418a36ba8cf9c8d906
IvanIvanovich] <2743@mail.ru>
2026-06-03 19:40:01 +0500

    Fix GENRE_CHOICES error and add image_url

--- main/models.py
+++ main/models.py
@@ -2,16 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
+    # СПИСОК ЖАНРОВ - ДОБАВИТЬ ЭТО ПЕРЕД ПОЛЯМИ!
+    GENRE_CHOICES = [
+        ('pop', 'Поп'),
+        ('jazz', 'Джаз'),
+        ('rock', 'Рок'),
+        ('electronic', 'Электроника'),
+    ]
+
     artist = models.CharField('Исполнитель', max_length=200)
     title = models.CharField('Название альбома', max_length=200)
     year = models.IntegerField('Год выпуска', null=True, blank=True)
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
     stock = models.IntegerField('Количество на складе', default=1)
+    genre = models.CharField('Жанр', max_length=20, choices=GENRE_CHOICES, default='pop')
     image = models.ImageField('Фото', upload_to='products/', blank=True, null=True)
+    image_url = models.URLField('Ссылка на фото (из интернета)', blank=True, null=True)
     created_at = models.DateTimeField('Дата добавления', auto_now_add=True)
 
     def __str__(self):
         return f"{self.artist} - {self.title}"
+
+    def get_image_url(self):
+        if self.image_url:
+            return self.image_url
+        elif self.image:
+            return self.image.url
+        return 'https://via.placeholder.com/300x300?text=No+Image'
 
     class Meta:
         verbose_name = 'Товар'
